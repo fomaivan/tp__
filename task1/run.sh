ab41c1f189690605f20ac5b1ed5f1c3260befe5a
Ivan Fomin <fomin.id@phystech.edu>
2022-03-04 01:02:28 +0300

    .

--- task1/run.sh
+++ task1/run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
-in_fold back_fold arch_name exten
-
 while [ -n "$1"]
 do
 case "$1" in
---input_folder) in_fold=$2
+--input_folder) in_fold="$2"
 ;;
---extension) exten=$2
+--extension) exten="$2"
 ;;
---backup_folder) back_fold=$2
+--backup_folder) back_fold="$2"
 ;;
---backup_archive_name) arch_name=$2
+--backup_archive_name) arch_name="$2"
 ;;
 esac
 shift
