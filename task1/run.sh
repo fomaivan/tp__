2383514b99e5d3075ecdf98d931cf098573a2d3f
Ivan Fomin <fomin.id@phystech.edu>
2022-03-04 00:00:48 +0300

    .

--- task1/run.sh
+++ task1/run.sh
@@ -4,9 +4,13 @@ while [ -n "$1"]
 do
 case "$1" in
 --input_folder) in_fold = $2
+;;
 --extension) exten = $2
+;;
 --backup_folder) back_fold = $2
+;;
 --backup_archive_name) arch_name = $2
+;;
 shift
 shift
 done
