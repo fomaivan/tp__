636e1a2e25551588205cd49462bcf1b7d293d35e
Ivan Fomin <fomin.id@phystech.edu>
2022-03-04 18:06:33 +0300

    .

--- task1/run.sh
+++ task1/run.sh
@@ -10,13 +10,12 @@ do
         back_fold="$2" ;;
     --backup_archive_name)
         arch_name="$2" ;;
-        *)
+    *)
     esac
     shift
     shift
 done
-cd $in_fold
 mkdir ./$back_fold
 find $in_fold -path "./$back_fold/*" -prune -o -name "*.$exten" -exec cp --parents "{}" ./$back_fold \;
-tar -czf $arch_name.tar.gz ./$back_fold
+tar -czf $arch_name ./$back_fold
 echo "done"
