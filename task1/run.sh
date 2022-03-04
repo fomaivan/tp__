2b8d49893bbebbb07df8818d79d829e9a5b8ee86
Ivan Fomin <fomin.id@phystech.edu>
2022-03-04 13:46:09 +0300

    .

--- task1/run.sh
+++ task1/run.sh
@@ -3,18 +3,21 @@ while [ -n "$1" ]
 do
     case "$1" in
         *input_folder)
-            in_fold="$2" ;;
+            in_fold="$2"
+            ;;
         *extension)
-            exten="$2" ;;
+            exten="$2"
+            ;;
         *backup_folder)
-            back_fold="$2" ;;
+            back_fold="$2"
+            ;;
         *backup_archive_name)
-            arch_name="$2" ;;
+            arch_name="$2"
+            ;;
         *)
     esac
-    shift 2
+    shift shift
 done
-
 mkdir ./$back_fold
 find $in_fold -path "./$back_fold/*" -prune -o "*.$exten" -exec cp --parents "{}" ./$back_fold \;
 tar -czf $arch_name ./$back_fold
