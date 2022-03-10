63cdfe36d7b455781ec1f4d5b1ddc9558c2751f2
Ivan Fomin <fomin.id@phystech.edu>
2022-03-10 22:30:49 +0300

    Subtotal

--- task2/A/preparing.py
+++ task2/A/preparing.py
@@ -1,2 +1,3 @@
 my_file = open("index.h", "w")
+my_file.write("bool bollian = false;")
 my_file.close()
---
+++ task2/C/testing.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "main.h"
+
+TEST(TEST_lib) {
+  ASSERT_EQ(F(), true);
+}
+
+TEST(TEST_index) {
+  ASSERT_EQ(bollian, false);
+}
+
