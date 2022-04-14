40712a9ead8e947f6fe3ba134b2ad5a2a2a9c611
Ivan Fomin <fomin.id@phystech.edu>
2022-04-14 12:22:49 +0300

    del c++

--- task4/tests/01-simple-library/AddTestCase.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by akhtyamovpavel on 5/1/20.
-//
-
-#include "AddTestCase.h"
-#include "Functions.h"
-
--- task4/tests/01-simple-library/LeapTestCase.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by akhtyamovpavel on 5/1/20.
-//
-
-#include "LeapTestCase.h"
-
-#include <Functions.h>
-
-
