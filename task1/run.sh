c631e84a2b58fd311a40224f1ae75e2802e5d774
Ivan Fomin <fomin.id@phystech.edu>
2022-04-22 21:11:23 +0300

    add dir task1

---
+++ task1/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while [ -n "$1" ]
+do
+  case "$1" in
+    --input_folder)
+        in_fold="$2" ;;
+    --extension)
+        exten="$2" ;;
+    --backup_folder)
+        back_fold="$2" ;;
+    --backup_archive_name)
+        arch_name="$2" ;;
+    *)
+    esac
+    shift
+    shift
+done
+mkdir ./$back_fold
+find $in_fold -path "./$back_fold/*" -prune -o -name "*.$exten" -exec cp --parents "{}" ./$back_fold \;
+tar -czf $arch_name ./$back_fold
+echo "done"
